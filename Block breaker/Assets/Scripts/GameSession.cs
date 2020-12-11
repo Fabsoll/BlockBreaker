9df9c4276b785f8550634f9aa5e10977c1d62e49
Fabsoll <Tixishow@gmail.com>
2020-12-11 20:36:00 +0100

    Block Breaker

---
+++ Block breaker/Assets/Scripts/GameSession.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+
+    // config params
+    [SerializeField] int pointsPerBlockDestroyed = 20;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    //Level uroven = FindObjectOfType<Level>();
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gameSpeed = 0.7f;
+        Time.timeScale = gameSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+            ResetGame();
+        }
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+}
---
+++ Block breaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed;
+
+    [SerializeField] int currentScore = 0;
+    [SerializeField] int pointsPerBlockDestroyed = 2;
+    [SerializeField] TextMeshProUGUI ScoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+
+    int levelNumber = 1;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetlevelNumber()
+    {
+        return levelNumber;
+    }
+
+    private void Start()
+    {
+        ScoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gameSpeed = 0.85f;
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore = currentScore + pointsPerBlockDestroyed;
+        ScoreText.text = currentScore.ToString();
+    }
+    public void ScoreReset()
+    {
+        Destroy(gameObject);
+    }
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+}
